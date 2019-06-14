7eae03d1720b91aed619dd70864364a6e15ef328
Miriam Armero <norma.armero@gmail.com>
2019-06-14 14:12:43 +0200

    añadido de home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,6 +6,7 @@ function home(){
         <h1>MARVEL HOME</h1>
         <Link to="/characters">Personajes</Link>
         <Link to="/comics">Comics</Link>
+        <img src="https://image.redbull.com/rbcom/052/2018-04-24/28ff870a-78f4-40cc-8152-988695017fe0/0012/0/0/121/602/1024/1500/1/cronologia-certa-para-assistis-aos-filmes-da-marvel.png" alt=""/>
     </div>
 }
 
